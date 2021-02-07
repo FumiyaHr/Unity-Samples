d5f206f073c8a90e99723193de1191d707a11a62
FumiyaHr <Fumiya.Hr@gmail.com>
2021-02-07 22:39:13 +0900

    Add hololens/PicturesLibraryAccess

---
+++ hololens/PicturesLibraryAccess/Assets/Scripts/Accessor.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if WINDOWS_UWP
+using System;
+using Windows.Storage;
+using System.Collections.Generic;
+#endif
+
+public class Accessor : MonoBehaviour
+{
+    [SerializeField]
+    private Text uiText;
+
+    private async void Start()
+    {
+        uiText.text = "test";
+        StringBuilder outputText = new StringBuilder();
+        outputText.AppendLine("KnownFolders.PicturesLibrary");
+#if WINDOWS_UWP
+        try
+        {
+            StorageFolder picturesFolder = KnownFolders.PicturesLibrary;
+            
+            IReadOnlyList<StorageFile> fileList = await picturesFolder.GetFilesAsync();
+            
+            outputText.AppendLine("Files:");
+            foreach (StorageFile file in fileList)
+            {
+                outputText.Append(file.Name + "\n");
+            }
+        }
+        catch (Exception e)
+        {
+            outputText.AppendLine(e.Message);
+        }
+#endif
+        uiText.text = outputText.ToString();
+    }
+}
---
+++ hololens/PicturesLibraryAccess/README.md
@@ -0,0 +1,17 @@
+# PicturesLibraryAccess
+
+## 環境
+
+- Unity 2019.4.19f1
+- Microsoft Mixed Reality Toolkit v2.5.4
+    - Microsoft.MixedReality.Toolkit.Unity.Foundation.2.5.4.unitypackage
+- Visual Studio 2019
+- HoloLens 2 Emulator 10.0.19041.1134
+
+## 参考サイト
+
+1. [プロジェクトの初期化と最初のアプリケーションの配置](https://docs.microsoft.com/ja-jp/windows/mixed-reality/develop/unity/tutorials/mr-learning-base-02)
+2. [HoloLens エミュレーターを使用する](https://docs.microsoft.com/ja-jp/windows/mixed-reality/develop/platform-capabilities-and-apis/using-the-hololens-emulator)
+3. [Universal Windows Platform Player Settings](https://docs.unity3d.com/ja/2019.4/Manual/class-PlayerSettingsWSA.html#Capabilities)
+4. [ファイルとフォルダーの列挙と照会](https://docs.microsoft.com/ja-jp/windows/uwp/files/quickstart-listing-files-and-folders)
+5. [空間上にテキストパネルを表示するHoloLensアプリを作成する](https://bluebirdofoz.hatenablog.com/entry/2019/03/09/143711)
